7f84b95f2d3a43c28ce88db85dd86dce7877488d
mrrobot <wcgiraldo0@gmail.com>
2024-05-02 23:35:42 -0500

    Signals Model

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-labs',
@@ -15,7 +15,7 @@ export class LabsComponent {
     'Crear proyecto',
     'Crear componentes',
   ];
-  name = 'will';
+  name = signal('will');
   age = 18;
   disabled = true;
   img = 'https://w3schools.com/howto/img_avatar.png';
@@ -31,7 +31,9 @@ export class LabsComponent {
   }
 
   changeHandler(event: Event) {
-    console.log(event);
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue);
   }
 
   keydownHandler(event: KeyboardEvent) {
