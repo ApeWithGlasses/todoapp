e3197898b2098ae432dcec7c574a50f02623a4a2
mrrobot <wcgiraldo0@gmail.com>
2024-05-02 23:51:16 -0500

    How to add and delete tasks with signals

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -15,4 +15,19 @@ export class HomeComponent {
     'Crear componentes',
     'Crear servicio',
   ]);
+
+  // How to add a new task into tasks with signals
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    // Recieves his last state and create a new state with the new task at the end of the list
+    this.tasks.update((tasks) => [...tasks, newTask]);
+    input.value = '';
+  }
+
+  // How to delete a task
+  // Recieves the position of the element
+  deleteTask(index: number) {
+    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
+  }
 }
